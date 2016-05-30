22dce85e67abf086b8299b4e6b7fb70e83e87aab
LCHAgithub <739092065@qq.com>
2016-05-30 19:22:53 +0800

    1,版本1

---
+++ src/com/lc/mobilesafe/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.lc.mobilesafe;
+
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SplashActivity extends Activity {
+
+	private TextView tv_splash_version;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		tv_splash_version = (TextView) findViewById(R.id.tv_splash_version);
+		tv_splash_version.setText("版本号:"+getVersion());
+	}
+	/**
+	 * 获取版本号
+	 */
+	private String getVersion(){
+		PackageManager pm = getPackageManager();
+		try {
+			PackageInfo packageInfo = pm.getPackageInfo(getPackageName(), 0);
+			return packageInfo.versionName;
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+}
